670cbd3218fae4d8c761aa98952777e8ca346de5
Begad2 <begadwael53@gmail.com>
2025-03-23 15:13:52 +0200

    update, delete, list, add is done

--- cli_v/CLI_TO.py
+++ cli_v/CLI_TO.py
@@ -29,7 +29,7 @@ if os.path.exists(file_name) and os.path.getsize(file_name)>0:
   with open(file_name, "r") as f:
     tasks = json.load(f)
     # print(tasks[0])
-    # index = tasks[-1].id + 1
+    index = tasks[-1]["id"] + 1
 else:
   tasks = []
 
@@ -43,12 +43,47 @@ add_parser = subparsers.add_parser("add", help="Add a new task")
 add_parser.add_argument("name")
 add_parser.add_argument("description")
 
+# Update command
+update_parser = subparsers.add_parser("update", help="Update a task")
+update_parser.add_argument("id")
+update_parser.add_argument("description")
+
+
+# Delete command
+delete_parser = subparsers.add_parser("delete", help="Delete a task")
+delete_parser.add_argument("id")
+# List command
+list_parser = subparsers.add_parser("list", help="List tasks")
+
 args = parser.parse_args()
 
 if args.command == "add":
   temp = Task(index, args.name, args.description, datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
+  index += 1
   tasks.append(temp.to_dict())
   with open (file_name, "w") as f1:
     json.dump(tasks,f1,indent=4)
+
+elif args.command == "update":
+  for i in range(len(tasks)):
+    if tasks[i]["id"] == int(args.id):
+      tasks[i]["description"] = args.description
+  
+  for i in range(len(tasks)):
+    print(tasks[i])
+
+  
+elif args.command == "delete":
+  # tasks.pop(int(args.id))
+  for i in range(len(tasks)):
+    if tasks[i]["id"] == int(args.id):
+      tasks.pop(i)
+  
+  for i in range(len(tasks)):
+    print(tasks[i])
+elif args.command == "list":
+  for i in range(len(tasks)):
+    print(tasks[i])
+    
 
 # print(Tasks[0])
